ee30679e7d5fccb15f8afb4ea1faf0ec257d57b8
kat28 <kategodinezd@gmail.com>
2022-12-15 02:27:50 -0600

    update varios

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,17 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Menu Sticky
    */
-  const selectHeader = document.querySelector('#header');
+  const selectHeader = document.querySelector('#header'); 
   if (selectHeader) {
     let headerOffset = selectHeader.offsetTop;
     let nextElement = selectHeader.nextElementSibling;
 
     const headerFixed = () => {
       if ((headerOffset - window.scrollY) <= 0) {
-        selectHeader.classList.add('sticked');
+        selectHeader.classList.add('sticked'); 
         if (nextElement) nextElement.classList.add('sticked-header-offset');
       } else {
-        selectHeader.classList.remove('sticked');
+        selectHeader.classList.remove('sticked'); 
         if (nextElement) nextElement.classList.remove('sticked-header-offset');
       }
     }
